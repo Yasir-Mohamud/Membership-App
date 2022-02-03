2afbfeafa32fc4f49a8e8246f64efbc16f5c2ac1
Yasir-Mohamud <ymohamed018@gmail.com>
2022-02-02 21:01:55 -0800

    daily commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Register.css";
 import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export default function Register() {
   const [registerUser, setRegisterUser] = useState({
@@ -10,7 +12,10 @@ export default function Register() {
     password: "",
     confirmPassword: "",
   });
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const [isRegistered, setIsRegistered] = useState(false);
   const [message, setMessage] = useState(" You are registered");
   const [error, setError] = useState("");
@@ -48,81 +53,118 @@ export default function Register() {
       console.log(response.data);
     });
     // set the setRegisterUser to have empty string to make the form as well as allow others to register
-    // setRegisterUser({
-    //   name: "",
-    //   email: "",
-    //   phoneNumber: "",
-    //   password: "",
-    //   confirmPassword: "",
-    // });
+    setRegisterUser({
+      name: "",
+      email: "",
+      phoneNumber: "",
+      password: "",
+      confirmPassword: "",
+    });
     setError("");
   }
 
   return (
-    <div className="register--form">
-      {error && <p style={{ color: "red" }}> {error}</p>}
+    <>
+      <div className="register--form">
+        {error && <p style={{ color: "red" }}> {error}</p>}
 
-      <form onSubmit={handleSubmit}>
-        <br />
-        <label htmlFor="name"> NAME</label>
-        <br />
-        <input
-          id="name"
-          type="text"
-          name="name"
-          value={registerUser.name}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label htmlFor="email"> EMAIL</label>
-        <br />
-        <input
-          id="email"
-          type="email"
-          name="email"
-          value={registerUser.email}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label htmlFor="phoneNumber">PHONE NUMBER</label>
-        <br />
-        <input
-          id="phoneNumber"
-          type="tel"
-          placeholder="123-45-678"
-          name="phoneNumber"
-          value={registerUser.phoneNumber}
-          onChange={handleChange}
-          pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-          required
-        />
-        <br />
-        <label htmlFor="password"> PASSWORD </label>
-        <br />
-        <input
-          id="password"
-          type="password"
-          name="password"
-          value={registerUser.password}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <label htmlFor="confirmPassword"> CONFIRM PASSWORD</label>
-        <br />
-        <input
-          id="confirmPassword"
-          type="password"
-          name="confirmPassword"
-          value={registerUser.confirmPassword}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <button> REGISTER</button>
-      </form>
-    </div>
+        <form onSubmit={handleSubmit}>
+          <br />
+          <label className="form--label" htmlFor="name">
+            NAME
+          </label>
+          <br />
+          <input
+            className="form--input"
+            id="name"
+            type="text"
+            name="name"
+            value={registerUser.name}
+            onChange={handleChange}
+            placeholder=" example : John Doe"
+            pattern=""
+            required
+          />
+          <br />
+          <label className="form--label" htmlFor="email">
+            EMAIL
+          </label>
+          <br />
+          <input
+            className="form--input"
+            id="email"
+            type="email"
+            name="email"
+            value={registerUser.email}
+            onChange={handleChange}
+            placeholder=" example : JohnDoe@email.com"
+            required
+          />
+          <br />
+          <label className="form--label" htmlFor="phoneNumber">
+            PHONE NUMBER
+          </label>
+          <br />
+          <input
+            className="form--input"
+            id="phoneNumber"
+            type="tel"
+            placeholder=" example : 123-45-678"
+            name="phoneNumber"
+            value={registerUser.phoneNumber}
+            onChange={handleChange}
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            required
+          />
+          <br />
+          <label className="form--label" htmlFor="password">
+            PASSWORD
+          </label>
+          <br />
+          <input
+            className="form--input"
+            id="password"
+            type="password"
+            name="password"
+            value={registerUser.password}
+            onChange={handleChange}
+            required
+          />
+          <br />
+          <label className="form--label" htmlFor="confirmPassword">
+            CONFIRM PASSWORD
+          </label>
+          <br />
+          <input
+            className="form--input"
+            id="confirmPassword"
+            type="password"
+            name="confirmPassword"
+            value={registerUser.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+          <br />
+          <button className="form--button" onClick={handleShow}>
+            {" "}
+            REGISTER
+          </button>
+        </form>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </>
   );
 }
