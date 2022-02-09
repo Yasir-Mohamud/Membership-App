55937383883c93c26e77b7028a0ed769216039f9
Yasir-Mohamud <ymohamed018@gmail.com>
2022-02-08 21:49:25 -0800

    daily commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,44 @@
 const router = require("express").Router();
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
+const { GridFsStorage } = require("multer-gridfs-storage");
+require("dotenv").config();
+const uri = process.env.ATLAS_URI;
+const mongoose = require("mongoose");
 
-const { monke: gfs } = require("../server.js");
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+let gfs;
+connection.once("open", () => {
+  // init stream
+  gfs = new mongoose.mongo.GridFSBucket(connection.db, {
+    bucketName: "users",
+  });
+});
+
+const storage = new GridFsStorage({
+  url: uri,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "users",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
+// const { monke: gfs } = require("../server.js");
 let User = require("../models/user.model");
-const upload = require("../middleware/upload");
 
 // creates user
 router.route("/add").post(upload.single("image"), (req, res) => {
@@ -28,24 +64,40 @@ router.route("/add").post(upload.single("image"), (req, res) => {
 router.route("/:email").get(async (req, res) => {
   const user = await User.findOne({ email: req.params.email });
   console.log("user revi", user.image);
-  console.log("gfs", gfs().files);
-  gfs().files.findOne({ filename: user.image }, (err, file) => {
-    console.log("getting h343 !!!!");
-    if (!file || file.length === 0) {
-      return res.status(404).json(`${err} file not found`);
-    }
+  const file = gfs
+    .find({
+      filename: user.image,
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist",
+        });
+      }
+      gfs.openDownloadStreamByName(user.image).pipe(res);
+    });
+  user.image = res
 
-    // if (file.contentType === "image/jpg") {
-    const readstream = gfs().createReadStream({ filename: user.image });
-    readstream.pipe(res);
-    console.log("body: %j", res);
-    // }
-  });
-
-  //   User.image = res
-  // .then((user) => res.json(user))
-  // .catch((err) => res.status(400).json("Error: " + err));
+    .save()
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
+
+//   gfs.find({ filename: user.image }, (err, file) => {
+//     console.log("getting h343 !!!!");
+//     if (!file || file.length === 0) {
+//       return res.status(404).json(`${err} file not found`);
+//     }
+// console.log("gfs", );
+// if (file.contentType === "image/jpg") {
+// const readstream = gfs().createReadStream(user.image);
+// readstream.pipe(res);
+// console.log("body: %j", res);
+// }
+// });
+
+// .then((user) => res.json(user))
+// .catch((err) => res.status(400).json("Error: " + err));
 
 router.route("/").get((req, res) => {
   User.find()
